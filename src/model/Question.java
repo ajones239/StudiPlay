14195f8f114d94cf26c16ac1c1e1f4689c183622
Aidan Jones <arjones239@protonmail.ch>
2020-03-18 20:05:36 -0700

    Created Question class, removed dummy /model/Test class

---
+++ src/model/Question.java
@@ -0,0 +1,28 @@
+package model;
+
+/*
+ * This class is used to model questions that users will be asked. Specific subjects 
+ * 	(ie AlgebraQuestion) should extend this class.
+ */
+public abstract class Question {
+	protected String text;		// text of the actual question
+	protected Object answer;	// question answer. If answer is numeric, use equivalent wrapper class (ie Integer)
+	
+	/*
+	 * @param text text the question asks
+	 * @param answer the answer to the question
+	 */
+	public Question(String text, Object answer) {
+		this.text = text;
+		this.answer = answer;
+	}
+	
+	/*
+	 * @param attempted answer to the question
+	 * @return true if the attempt matches the answer
+	 */
+	protected boolean checkAnswer(Object attempt) {
+		return attempt.equals(answer);
+	}
+
+}
