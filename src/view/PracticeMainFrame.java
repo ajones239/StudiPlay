c221ef7db1b5663247e34658cc681ac876c6e746
Aidan Jones <arjones239@protonmail.ch>
2020-04-11 23:53:35 -0700

    Updated PracticeMainFrame and created QuestionPanel.

--- src/view/PracticeMainFrame.java
+++ src/view/PracticeMainFrame.java
@@ -1,11 +1,32 @@
 package view;
 
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import java.awt.*;
 
 public class PracticeMainFrame extends JFrame {
 	
+	JPanel subjectSelectionPane;
+	JPanel contentPane;
+	
 	public PracticeMainFrame() {
+		setBounds(100, 100, 671, 441); // What are these numbers?
 
+		subjectSelectionPane = new JPanel();
+		subjectSelectionPane.setLayout(new BoxLayout(subjectSelectionPane, BoxLayout.Y_AXIS));
+		subjectSelectionPane.add(new JButton("cat1"));
+		subjectSelectionPane.add(new JButton("cat2"));
+		subjectSelectionPane.add(new JButton("cat3"));
+		subjectSelectionPane.add(new JButton("cat4"));
+
+		contentPane = new JPanel();
+		contentPane.setLayout(new BorderLayout());
+		JLabel defaultText = new JLabel("Select a subject to get started!");
+		defaultText.setHorizontalAlignment(JLabel.CENTER);
+		contentPane.add(defaultText, BorderLayout.CENTER);
+
+		setLayout(new BorderLayout());
+		add(contentPane, BorderLayout.CENTER);
+		add(subjectSelectionPane, BorderLayout.WEST);
+	
 	}
 }
---
+++ src/view/QuestionPanel.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+import model.Question;
+
+public class QuestionPanel extends JPanel {
+
+	Question currentQuestion;
+	
+	QuestionPanel() {
+		
+	}
+}
