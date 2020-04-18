268b4d16799140c935fd22b1f77bcce13bcaf74a
Aidan Jones <arjones239@protonmail.ch>
2020-04-17 17:28:55 -0700

    Created basic QuestionPanel and integrated it into PracticeMainFrame

--- src/view/PracticeMainFrame.java
+++ src/view/PracticeMainFrame.java
@@ -1,26 +1,42 @@
 package view;
 
 import javax.swing.*;
+
+import model.Question;
+
 import java.awt.*;
+import java.awt.event.*;
 
 public class PracticeMainFrame extends JFrame {
 	
 	JPanel subjectSelectionPane;
 	JPanel contentPane;
+	QuestionPanel qpanel;
+	JLabel defaultText;
 	
 	public PracticeMainFrame() {
 		setBounds(100, 100, 671, 441); // What are these numbers?
 
 		subjectSelectionPane = new JPanel();
 		subjectSelectionPane.setLayout(new BoxLayout(subjectSelectionPane, BoxLayout.Y_AXIS));
-		subjectSelectionPane.add(new JButton("cat1"));
-		subjectSelectionPane.add(new JButton("cat2"));
-		subjectSelectionPane.add(new JButton("cat3"));
-		subjectSelectionPane.add(new JButton("cat4"));
+		JButton topic1 = new JButton("Basic Math");
+		JButton topic2 = new JButton("Algebra");
+		JButton topic3 = new JButton("Quantum Chemistry");
+		JButton topic4 = new JButton("Language Arts");
+		
+		subjectSelectionPane.add(topic1);
+		subjectSelectionPane.add(topic2);
+		subjectSelectionPane.add(topic3);
+		subjectSelectionPane.add(topic4);
+		
+		topic1.addActionListener(event -> replaceSubject(topic1.getText()));
+		topic2.addActionListener(event -> replaceSubject(topic2.getText()));
+		topic3.addActionListener(event -> replaceSubject(topic3.getText()));
+		topic4.addActionListener(event -> replaceSubject(topic4.getText()));
 
 		contentPane = new JPanel();
 		contentPane.setLayout(new BorderLayout());
-		JLabel defaultText = new JLabel("Select a subject to get started!");
+		defaultText = new JLabel("Select a subject to get started!");
 		defaultText.setHorizontalAlignment(JLabel.CENTER);
 		contentPane.add(defaultText, BorderLayout.CENTER);
 
@@ -28,5 +44,38 @@ public class PracticeMainFrame extends JFrame {
 		add(contentPane, BorderLayout.CENTER);
 		add(subjectSelectionPane, BorderLayout.WEST);
 	
+	}
+	
+	private void replaceSubject(String subject) {
+		try {
+			for (Component c: contentPane.getComponents()) {
+				if (c instanceof JLabel) {
+					contentPane.remove(c);
+					contentPane.revalidate();
+					contentPane.repaint();
+					break;
+				}
+			}
+		}
+		catch (NullPointerException e) {
+			e.printStackTrace();
+			// This will be Null every time this method is called except the first
+		}
+		try {
+			for (Component c: contentPane.getComponents()) {
+				if (c instanceof QuestionPanel) {
+					contentPane.remove(c);
+					contentPane.revalidate();
+					contentPane.repaint();
+					break;
+				}
+			}
+		}
+		catch (NullPointerException e) {
+			e.printStackTrace();
+			// No action required, this will throw a NullPointerException the first time
+		}
+		qpanel = new QuestionPanel(new Question("Basic Math", "1 + 2","3", "5", "4", "2", 1, "N/A"));
+		contentPane.add(qpanel, BorderLayout.CENTER);
 	}
 }
--- src/view/QuestionPanel.java
+++ src/view/QuestionPanel.java
@@ -1,13 +1,33 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 import model.Question;
 
 public class QuestionPanel extends JPanel {
 
 	Question currentQuestion;
 	
-	QuestionPanel() {
-		
+	QuestionPanel(Question q) {
+		currentQuestion = q;
+		setLayout(new BorderLayout());
+		JLabel qTitle = new JLabel(q.getQuestion());
+		qTitle.setHorizontalAlignment(JLabel.CENTER);
+		this.add(qTitle, BorderLayout.NORTH);
+		JRadioButton option1 = new JRadioButton("a)");
+		JRadioButton option2 = new JRadioButton("b)");
+		JRadioButton option3 = new JRadioButton("c)");
+		JRadioButton option4 = new JRadioButton("d)");
+		ButtonGroup group = new ButtonGroup();
+		group.add(option1);
+		group.add(option2);
+		group.add(option3);
+		group.add(option4);
+		add(option1, BorderLayout.SOUTH);
+		add(option2, BorderLayout.SOUTH);
+		add(option3, BorderLayout.SOUTH);
+		add(option4, BorderLayout.SOUTH);
+		JButton submitButton = new JButton("submit");
+		add(submitButton, BorderLayout.SOUTH);
 	}
 }
